9c4e3a55ba94cf2bfb50e566ab2c415532c3a48e
tezov <tezov.app@gmail.com>
2026-01-13 22:33:14 +0100

    fix/marketing-version

--- vars/getIsSnapshot.groovy
+++ vars/getIsSnapshot.groovy
@@ -3,7 +3,7 @@ def call(
         String jobName = env.JOB_NAME,
         String buildNumber = env.BUILD_NUMBER
 ) {
-    def libsFile = "${project.path(agent, jobName, buildNumber)}/libs.versions.toml"
+    def libsFile = "${project.path(agent, jobName, buildNumber)}/tuucho/libs.versions.toml"
     def libsContent = readFile libsFile
     def matcher = libsContent =~ /(?m)^(?!#)\s*isSnapshot\s*=\s*["'](.+)["']\s*$/
     if (!matcher) {
