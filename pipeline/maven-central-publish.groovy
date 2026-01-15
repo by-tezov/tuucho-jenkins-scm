69f754ec30391be686906a534721a658d5bd5a73
tezov <tezov.app@gmail.com>
2026-01-15 01:05:47 +0100

    fix maven publish

--- pipeline/maven-central-publish.groovy
+++ pipeline/maven-central-publish.groovy
@@ -211,7 +211,7 @@ pipeline {
                     ]) {
                         withEnv(["MAVEN_SIGNING_KEY=" + readFile(MAVEN_SIGNING_KEY_FILE)]) {
                             sourceEnv {
-                                runGradleTask("rootPublishReleaseToMavenLocal")
+                                runGradleTask('project', 'rootPublishReleaseToMavenLocal')
                             }
                         }
                     }
